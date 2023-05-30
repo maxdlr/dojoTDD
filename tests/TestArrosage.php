4f7156a97dd99a426829b435b1d34e0b92868a3c
maxdlr <105718327+maxdlr@users.noreply.github.com>
2023-05-30 11:22:17 +0200

    push

---
+++ tests/TestArrosage.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once '../src/arrosage.php';
+
+use PHPUnit\Framework\TestCase;
+
+final class TestArrosage extends TestCase
+{
+    public function testArrosage(){
+
+        $this->assertTrue(true);
+    }
+}
